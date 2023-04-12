1ac457b3ac46345457f15c9e038eb934c6e4ede8
Habiba <habiba.elganainey@gmail.com>
2023-04-12 15:14:12 +0200

    Lab1

---
+++ Day1/task2/main.ts
@@ -0,0 +1,38 @@
+interface IAccount{
+    Date_of_opening:Date;
+
+    addCustomer();
+    removeCustomer();
+}
+class Account {
+    constructor(private Acc_no: number, private Balance: number){}
+
+    debitAmount(){}
+    creditAmount(){}
+    getBalance() {}
+}
+
+class Saving_Account extends Account implements IAccount {
+    constructor(Acc_no, Balance, private Min_Balance:number ,public Date_of_opening:Date){
+        super(Acc_no, Balance);
+    }
+    addCustomer() {
+        throw new Error("Method not implemented.");
+    }
+    removeCustomer() {
+        throw new Error("Method not implemented.");
+    }
+}
+
+class Current_Account extends Account implements IAccount{
+    constructor(Acc_no, Balance, private Interest_rate:number,public Date_of_opening: Date){
+        super(Acc_no, Balance);
+    }
+    addCustomer() {
+        throw new Error("Method not implemented.");
+    }
+    removeCustomer() {
+        throw new Error("Method not implemented.");
+    }
+    
+}
